89e62beae981bb4d6645bd1ed1cfa634cba0fd0f
Dustin Abhishek <dustiabhishek@73235@gmail.com>
2024-08-29 19:41:29 +0530

    changing get into put

--- routers/userRoutes.js
+++ routers/userRoutes.js
@@ -27,7 +27,7 @@ router.delete('/api/v1/user/delete/:ethereumId', deleteUserByEthereumId);
 router.get('/api/v1/refer/code/:referralCode', getUserByReferCode);
 router.put('/api/v1/refer/update/code/:referralCode',fetchDataUser)
 router.put('/api/v1/user/social/link/:ethereumId',socialLink)
-router.get('/api/v1/user/count/click/:ethereumId',clickCount)
+router.put('/api/v1/user/count/click/:ethereumId',clickCount)
 router.post('/api/v1/user/timer/count/:ethereumId',startCountdown)
 
 // click check
